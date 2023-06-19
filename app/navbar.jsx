aa3e11cf2c3608acd794e9542537f0080b4f2804
ChakornK <chakornk2007@gmail.com>
2023-06-18 17:07:07 -0700

    navbar blur

--- app/navbar.jsx
+++ app/navbar.jsx
@@ -2,7 +2,7 @@ export function NavBar() {
 	return (
 		<>
 			<div className="w-full h-16"></div>
-			<div className="w-full h-16 fixed top-0 left-0 bg-black border-b-2 border-[#222]"></div>
+			<div className="w-full h-16 fixed top-0 left-0 bg-[#0000] border-b-[1px] border-[#444] backdrop-blur-md backdrop-brightness-50"></div>
 		</>
 	);
 }
