2dca92a7ddeea13301210c7b6affdd2531c948bd
ChakornK <chakornk2007@gmail.com>
2023-06-18 18:11:45 -0700

    add 404 page

---
+++ app/404.jsx
@@ -0,0 +1,5 @@
+import { Custom404 } from "./errors/404/page";
+
+export default function Custom404Page() {
+	return <Custom404 />;
+}
---
+++ app/errors/404/page.jsx
@@ -0,0 +1,8 @@
+export default function Custom404() {
+	return (
+		<main className="bg-black h-screen w-screen flex flex-col justify-center items-center">
+			<p className="text-7xl">🙈</p>
+			<p className="text-7xl">404</p>
+		</main>
+	);
+}
