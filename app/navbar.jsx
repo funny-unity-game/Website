4913ad5b65a406e8e048a0e36687bc65f14cdf82
ChakornK <120770627+ChakornK@users.noreply.github.com>
2023-06-19 08:15:14 -0700

    make navbar go on top of everything else

--- app/navbar.jsx
+++ app/navbar.jsx
@@ -2,7 +2,7 @@ export default function NavBar() {
 	return (
 		<>
 			<div className="w-full h-16"></div>
-			<div className="w-full h-16 fixed top-0 left-0 bg-[#0000] border-b-[1px] border-[#444] backdrop-blur-md backdrop-brightness-50"></div>
+			<div className="w-full h-16 fixed top-0 left-0 bg-[#0000] border-b-[1px] border-[#444] backdrop-blur-md backdrop-brightness-50 z-50"></div>
 		</>
 	);
 }
