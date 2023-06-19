c345dcb7497887a5cd8ee2b0332bbcedbc2a3ca9
ChakornK <chakornk2007@gmail.com>
2023-06-18 17:25:12 -0700

    add source code links to footer

--- app/footer.jsx
+++ app/footer.jsx
@@ -1,17 +1,39 @@
 export function Footer() {
 	return (
-		<div className="px-48 py-12 bg-[#111] text-[#888] flex justify-between border-t-[1px] border-t-[#444]">
-			<div className="flex flex-col gap-4">
-				<div className="flex gap-4 items-center">
-					<img
-						src="/icon.png"
-						width={48}
-					/>
-					<p className="text-white font-normal text-2xl select-none">Mission: Monkey</p>
+		<div className="px-12 sm:px-24 py-12 bg-[#111] text-[#888] border-t-[1px] border-t-[#444] flex justify-center">
+			<div className="flex flex-col gap-12 max-w-5xl w-full">
+				<div className="flex flex-row items-start gap-32">
+					<div className="flex gap-4 items-center -translate-y-2">
+						<img
+							src="/icon.png"
+							width={48}
+						/>
+						<p className="text-white font-normal text-2xl select-none">Mission: Monkey</p>
+					</div>
+					<div className="text-sm leading-8">
+						<p className="text-white">Source code</p>
+						<p>
+							<a href="https://github.com/funny-unity-game/Mission-Monkey">Game</a>
+						</p>
+						<p>
+							<a href="https://github.com/funny-unity-game/Website">Website</a>
+						</p>
+					</div>
 				</div>
-				<p>©️ 2023 Lemon Studios</p>
+				<div className="flex flex-row justify-between items-end">
+					<div className="flex flex-col gap-4">
+						<p>©️ 2023 Lemon Studios</p>
+					</div>
+					<div className="flex flex-col gap-4">
+						<a
+							href="https://github.com/funny-unity-game"
+							className="text-2xl"
+						>
+							<i class="bi bi-github"></i>
+						</a>
+					</div>
+				</div>
 			</div>
-			<div></div>
 		</div>
 	);
 }
