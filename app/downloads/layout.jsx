ddb337ba73cabcc6f8ae0e540d4f2373908c9354
ChakornK <chakornk2007@gmail.com>
2023-06-20 18:26:52 -0700

    update meta tags for downloads page

---
+++ app/downloads/layout.jsx
@@ -0,0 +1,24 @@
+import "./globals.css";
+
+import NavBar from "./navbar.jsx";
+import Footer from "./footer.jsx";
+
+export const metadata = {
+	title: "Download Mission: Monkey",
+	description: "Download the funny game made by at least 2 grade 10 students for a school project"
+};
+
+export default function RootLayout({ children }) {
+	return (
+		<html lang="en">
+			<head>
+				<base target="_blank"></base>
+			</head>
+			<body>
+				<NavBar />
+				{children}
+				<Footer />
+			</body>
+		</html>
+	);
+}
