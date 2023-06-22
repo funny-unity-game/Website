e2652baabfec6b80eb2b24c4779a7d1c2a5062ef
ChakornK <120770627+ChakornK@users.noreply.github.com>
2023-06-22 14:50:56 -0700

    Revert "oops fixed embed i think"
    This reverts commit 9c00a3ca32f1ec774b5e025182e56062fc3fc510.

--- app/gallery/layout.jsx
+++ app/gallery/layout.jsx
@@ -2,7 +2,8 @@ import "../globals.css";
 
 export const metadata = {
 	title: "Mission: Monkey Screenshots",
-	description: "What does this funny game look like? 🤔"
+	description: "What does this funny game look like? 🤔",
+	image: "https://missionmonkey.netlify.app/icon.png"
 };
 
 export default function RootLayout({ children }) {
--- app/layout.jsx
+++ app/layout.jsx
@@ -5,7 +5,8 @@ import Footer from "../components/footer.jsx";
 
 export const metadata = {
 	title: "Mission: Monkey",
-	description: "A game for a school project created by at least 2 grade 10 students"
+	description: "A game for a school project created by at least 2 grade 10 students",
+	image: "https://missionmonkey.netlify.app/icon.png"
 };
 
 export default function RootLayout({ children }) {
@@ -13,10 +14,6 @@ export default function RootLayout({ children }) {
 		<html lang="en">
 			<head>
 				<base target="_blank"></base>
-				<meta property="og:image" content="https://missionmonkey.netlify.app/icon.png" />
-				<meta property="og:image:type" content="image/png" />
-				<meta property="og:image:width" content="128" />
-				<meta property="og:image:height" content="128" />
 			</head>
 			<body>
 				<NavBar />
