53d8f6df9118e96a9ccc28a8af8e7bb816771021
ChakornK <chakornk2007@gmail.com>
2023-06-21 19:23:20 -0700

    customize lightbox.js image gallery

--- app/gallery/page.jsx
+++ app/gallery/page.jsx
@@ -7,7 +7,13 @@ export default function Gallery() {
 	return (
 		<main>
 			<div className="p-8">
-				<SlideshowLightbox className="columns-sm gap-4 space-y-4">
+				<SlideshowLightbox
+					className="columns-sm gap-4 space-y-4"
+					showControls={false}
+					iconColor={"#fffa"}
+					backgroundColor={"#000b"}
+					showThumbnails={true}
+				>
 					<img
 						className="rounded-xl"
 						src="/screenshots/1.png"
